0051b042cf0b2d1ebd433f584af41a2c6a813eac
river.yb.shin <river.yb.shin@openedges.com>
2025-05-03 18:29:56 +0900

    debug: 환경변수 출력

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import { ResponsiveLayout } from "./components/ResponsiveLayout";
 import { UploadForm } from "./components/UploadForm";
 
 function App() {
+  console.log(import.meta.env.VITE_GOOGLE_CLIENT_ID);
+  console.log(import.meta.env.VITE_API_SERVER_URL);
+  console.log(import.meta.env.VITE_AUTHENTICATION_SERVER_URL);
   return (
     <div className="grid min-h-screen place-items-center bg-background px-4 text-foreground">
       <ResponsiveLayout left={<AuthButton />} right={<UploadForm />} />
