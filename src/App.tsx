e7d4881ee7ed13a95010c7fdb09fc93ea9201a5a
river.yb.shin <river.yb.shin@openedges.com>
2025-05-04 18:30:47 +0900

    feat: redirect 시 root 페이지로

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,18 @@
 import "./App.css";
 
-import { Route, Routes } from "react-router-dom";
-
 import { AuthButton } from "./components/AuthButton";
 import { ResponsiveLayout } from "./components/ResponsiveLayout";
 import { UploadForm } from "./components/UploadForm";
-import OAuthCallback from "./pages/oauth/OAuthCallback"; // 추가
-
-function Home() {
-  return (
-    <div className="grid min-h-screen place-items-center bg-background px-4 text-foreground">
-      <ResponsiveLayout left={<AuthButton />} right={<UploadForm />} />
-    </div>
-  );
-}
 
 function App() {
   console.log(import.meta.env.VITE_API_SERVER_URL);
   console.log(import.meta.env.VITE_GOOGLE_CLIENT_ID);
   console.log(import.meta.env.VITE_AUTHENTICATION_SERVER_URL);
+
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/oauth-callback" element={<OAuthCallback />} />
-    </Routes>
+    <div className="grid min-h-screen place-items-center bg-background px-4 text-foreground">
+      <ResponsiveLayout left={<AuthButton />} right={<UploadForm />} />
+    </div>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,13 @@
 import "./index.css";
-
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <App />
     </GoogleOAuthProvider>
   </StrictMode>,
 );
