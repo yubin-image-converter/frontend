7363d2b90da1f738f2c00ffa0e22656ad8a05af1
river.yb.shin <river.yb.shin@openedges.com>
2025-05-14 12:24:07 +0900

    fix: socket server url 추가

--- src/shared/lib/axiosInstance.ts
+++ src/shared/lib/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: import.meta.env.VITE_API_SERVER_URL + "/api",
-  withCredentials: false,
+  withCredentials: true,
 });
 
 // 앱 시작 시 localStorage에서 토큰 불러오기
