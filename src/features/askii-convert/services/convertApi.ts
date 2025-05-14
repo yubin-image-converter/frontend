ac7256f39bbe27d4eb391e6058d04309a6f3fd50
river.yb.shin <river.yb.shin@openedges.com>
2025-05-14 13:42:52 +0900

    fix: debug 직접헤더에 accessToken 추가, 로그 추가

--- src/features/askii-convert/services/convertApi.ts
+++ src/features/askii-convert/services/convertApi.ts
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 import axiosInstance from "@/shared/lib/axiosInstance";
 
 export async function fetchAsciiResult(
@@ -6,10 +8,20 @@ export async function fetchAsciiResult(
   try {
     const res = await axiosInstance.get("/converts/result", {
       params: { requestId },
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
     });
     return res.data.txtUrl;
   } catch (err) {
-    console.error("❌ ASCII 결과 요청 실패", err);
+    if (axios.isAxiosError(err)) {
+      console.error("❌ ASCII 결과 요청 실패:", {
+        status: err.response?.status,
+        data: err.response?.data,
+      });
+    } else {
+      console.error("❌ 알 수 없는 에러:", err);
+    }
     return null;
   }
 }
