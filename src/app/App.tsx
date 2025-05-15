d0d6d6bfc291e19088a7c43b66f0b67fd8cdecfe
river.yb.shin <river.yb.shin@openedges.com>
2025-05-15 12:33:57 +0900

    feat: jotai app.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,24 +6,27 @@ import { Toaster } from "sonner";
 import { Footer, Header, Main } from "@/shared/components";
 
 import { AuthProvider } from "./providers";
+import { JotaiStateProvider } from "./providers/JotaiProvider";
 
 function App() {
   return (
-    <div className="flex min-h-screen flex-col bg-black font-mono text-gray-100">
-      <Toaster
-        position="top-center"
-        toastOptions={{
-          unstyled: true,
-          className:
-            "bg-black text-green-300 border border-green-500 rounded-sm px-4 py-2 shadow-none font-mono text-sm leading-tight tracking-tight flex items-center gap-2",
-        }}
-      />
-      <Header />
+    <JotaiStateProvider>
       <AuthProvider>
-        <Main />
+        <div className="flex min-h-screen flex-col bg-black font-mono text-gray-100">
+          <Toaster
+            position="top-center"
+            toastOptions={{
+              unstyled: true,
+              className:
+                "bg-black text-green-300 border border-green-500 rounded-sm px-4 py-2 shadow-none font-mono text-sm leading-tight tracking-tight flex items-center gap-2",
+            }}
+          />
+          <Header />
+          <Main />
+          <Footer />
+        </div>
       </AuthProvider>
-      <Footer />
-    </div>
+    </JotaiStateProvider>
   );
 }
 
